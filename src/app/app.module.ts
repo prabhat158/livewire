284b95a9eb2e31d876155737515c9b2226a1e800
pritam-iitb <pritamkumariitb@gmail.com>
2019-09-08 00:36:14 +0530

    1st

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { WinnerComponent } from './winner/winner.component';
+import { SponsersComponent } from './sponsers/sponsers.component';
+import {RouterModule,Routes} from '@angular/router'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WinnerComponent,
+    SponsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // RouterModule.forRoot([
+    //   {path:'',component:HomeComponent},
+    //   {path:'sponsors',component:SponsersComponent},
+    //   {path:'winner',component:WinnerComponent},
+
+    // ])
   ],
   providers: [],
   bootstrap: [AppComponent]
